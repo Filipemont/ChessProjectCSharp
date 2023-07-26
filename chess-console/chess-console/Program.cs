011525d2db81363af64979ce34bcec060682dcdd
Filipe Monteiro <filipemonteiro73@gmail.com>
2023-07-26 19:28:55 -0300

    Projeto criado

---
+++ chess-console/chess-console/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace chess_console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
